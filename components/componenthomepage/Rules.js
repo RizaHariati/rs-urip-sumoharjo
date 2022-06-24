93a2bc81675fbfbf7ba21eaf3c50ffd143688fb5
Riza Hariati <76550345+RizaHariati@users.noreply.github.com>
2022-06-24 14:49:10 +0700

    fixing rule's font

--- components/componenthomepage/Rules.js
+++ components/componenthomepage/Rules.js
@@ -14,23 +14,23 @@ const Rules = () => {
           />
         </div>
         <h2 className="font-normal mb-2">Kami peduli pada Anda</h2>
-        <h4 className=" font-thin mb-5 leading-6">
+        <p className="mb-5">
           Cara terbaik untuk melindungi Anda dan orang-orang yang Anda kasihi
           adalah dengan melakukan vaksinasi serta boosternya. Mengikuti
           peraturan COVID TESTING, menggunakan masker, mencuci tangan dan
-          menjaga jarak.{" "}
-        </h4>
+          menjaga jarak.
+        </p>
         <h2 className="font-normal mb-2">Tata Cara Menjaga Keamanan Anda</h2>
-        <h4 className=" font-thin leading-6">
+        <p>
           Masker diwajibkan untuk memasuki area dan fasilitas manapun di dalam
           RS Urip Sumoharjo dan harus dipakai sepanjang waktu, sekalipun Anda
           sudah divaksinasi dan mendapatkan booster. Kami akan menyediakan
           Masker jika diperlukan.
-        </h4>
-        <h4 className=" font-thin leading-6">
+        </p>
+        <p>
           Syal, bandana, masker dengan lubang pernafasan, atau pelindung plastik
           muka, tidak bisa menggantikan masker standar.
-        </h4>
+        </p>
       </div>
     </div>
   );
